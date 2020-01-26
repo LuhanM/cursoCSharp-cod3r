6d8adba628d797cefcecd09c4ee2da2a6b7f0331
LuhanM <meireles.luhan@gmail.com>
2020-01-26 16:03:08 -0300

    Exemplo da estrutura de repetição foreach

---
+++ EstruturasDeControle/EstruturaForeach.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturasDeControle
+{
+    class EstruturaForeach
+    {
+        public static void Executar()
+        {
+            var palavra = "oi.";
+
+            foreach (var letra in palavra)
+            {
+                Console.WriteLine(letra);
+            }
+
+            var alunos = new string[] { "luhan", "josé", "paulo", "felipe" };
+            foreach (var aluno in alunos){
+                Console.WriteLine(aluno);
+            }
+        }
+    }
+}
