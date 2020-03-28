ab232a96584fb92b5c423cbfb23ac9ee69c933bd
LuhanM <meireles.luhan@gmail.com>
2020-03-28 19:46:37 -0300

    Classe Métodos com Retorno

---
+++ ClassesEMetodos/MetodosComRetorno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class CalculadoraComum
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+                       
+    }
+    
+    class CalculadoraCadeia
+    {
+        int memoria;
+        public CalculadoraCadeia Somar(int a)
+        {
+            memoria += a;
+            return this;
+        }
+        
+        public CalculadoraCadeia Multiplicar(int a)
+        {
+            memoria *= a;
+            return this;
+        }
+
+        public CalculadoraCadeia Imprimir()
+        {
+            Console.WriteLine(memoria);
+            return this;
+        }
+
+        public int Resultado()
+        {
+            return memoria;
+        }
+    }
+
+    class MetodosComRetorno
+    {
+        public static void Executar()
+        {
+            var calculadoraComum = new CalculadoraComum();
+            Console.WriteLine(calculadoraComum.Somar(1, 2));
+
+            var calculadoraCadeia = new CalculadoraCadeia();
+            var resultado = calculadoraCadeia.Somar(10).Multiplicar(2).Imprimir().Resultado();
+        }
+    }
+}
